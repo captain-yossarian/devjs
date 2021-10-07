405483f351dad2874ed09c2f81bfc1cb6a998b65
captain-yossarian <sergiybiluk@gmail.com>
2021-10-07 16:44:20 +0200

    add more info

--- 2_Generics.ts
+++ 2_Generics.ts
@@ -181,3 +181,4 @@ declare function baz(a: number[]): string;
 // pipe proposition https://github.com/tc39/proposal-pipeline-operator
 // pipe discussion https://github.com/tc39/proposal-pipeline-operator/issues/205
 // pipe summarize https://benlesh.com/posts/tc39-pipeline-proposal-hack-vs-f-sharp/
+// TS for FP https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes-func.html
